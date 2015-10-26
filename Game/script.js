ad75bd1fca692e0715c03fe05c394e9bbb625e52
Bryce Lively <bryce.lively@gmail.com>
2015-10-26 18:38:06 -0400

    functionality!

--- Game/script.js
+++ Game/script.js
@@ -2,31 +2,30 @@ $(document).ready(function() {
   console.log("jQuery Working");
   $("#Announcement").html("FLASH CARDS FOR NATIONAL CAPITALS");
   var numCorrect = 0;
-  $(".country").on("click", function() {
-    console.log("Click detected on " + $(this));
-    var identity = $(this);
 
-    function checkContent() {
-      var sign = prompt("What is the capital of " + $(identity).attr('id') + "?");
-      // The following two lines do not work. I do not know why (this) is not successfully referencing the element that has been clicked on.
-      //If I console.log($(".country")[0].firstChild.nextSibling.textContent.toLowerCase()) It correctly returns "canberra"  This probably has something to do with the way prompt works...
-      console.log($(this).firstChild.nextSibling.textContent.toLowerCase());
-      if (sign.toLowerCase() == $(this).firstChild.nextSibling.textContent.toLowerCase()) {
-        numCorrect++;
-        alert("Good Job!");
-        return true;
-      } else {
-        alert("Try Again...")
-        return false;
-      };
+  function checkContent(event) {
+    var identity = event.target;
+    console.log("identity set to: " + identity);
+    var user = prompt("What is the capital of " + event.target.id + "?");
+    var theCapital = event.target.firstChild.nextSibling.textContent.toLowerCase();
+    console.log("Target text: " + theCapital);
+    if (user.toLowerCase() == theCapital) {
+      numCorrect++;
+      alert("Good Job!");
+      return true;
+    } else {
+      alert("Try Again...")
+      return false;
     };
+  };
+  $(".country").on("click", function(event) {
+    console.log("Click detected on " + event.target.html);
     var retract = function retracting() {
       console.log("trying to retract...");
       $(this).closest('div').find('p').removeClass("pop");
     }.bind(this);
-    checkContent();
+    checkContent(event);
     console.log("popping out!");
-    console.log(this);
     $(this).closest('div').find('p').removeClass().addClass("pop");
     setTimeout(retract, 2000);
   });
