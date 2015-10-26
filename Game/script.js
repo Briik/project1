9c1396d2d8e6a4b6ba6ae8b3719ba5699863c5e6
Bryce Lively <bryce.lively@gmail.com>
2015-10-26 11:01:57 -0400

    cards add text when clicked, but need to make it disappear. rearrange content as paragraphs?

--- Game/script.js
+++ Game/script.js
@@ -1,12 +1,13 @@
 var FTAs = [("Australia"), ("Bahrain"), ("Chile"), ("Colombia"), ("DR-CAFTA"), ("Israel"), ("Jordan"), ("Korea"), ("Morocco"), ("NAFTA"), ("Oman"), ("Panama"), ("Peru"), ("Singapore"),];
 
-// var Announcement = document.querySelector("#Announcement").innerHTML = "There are currently " + (FTAs.length + 1) + " Free Trade Agreements in force for the United States.<br>Learn more about them here.";
 
 $(document).ready(function(){
     console.log("jQuery Working");
     $("#Announcement").html("There are currently " + (FTAs.length + 1) + " Free Trade Agreements in force for the United States.<br>Learn more about them here.");
     $(".country").on("click", function(){
       console.log("Click detected on " + $(this).html());
-      $(this).html("TESTTESTTEST");
+      var identity = $(this).id;
+      // $(this).append("<br>TESTTESTTEST");
+      // $(this)setTimeout($(this)., 5000)
     });
 });
