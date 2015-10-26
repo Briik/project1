bfc61a6d8e1b6ba316e3ee1e7b2d011122edec41
Bryce Lively <bryce.lively@gmail.com>
2015-10-26 15:54:21 -0400

    progress, need to link answers

--- Game/script.js
+++ Game/script.js
@@ -3,32 +3,29 @@ $(document).ready(function() {
   $("#Announcement").html("WHAT ARE YOU DOING, BRYCE?");
   $(".country").on("click", function() {
     console.log("Click detected on " + $(this).html());
-    var identity = $(this).id;
-    function checkContent() {
-      var sign = prompt("Ask for data here");
+    var identity = $(this);
 
+    function checkContent() {
+      var sign = prompt("What is the capital of " + $(identity).attr('id') + "?");
       if (sign.toLowerCase() == "correct") {
         alert("Good Job!");
         return true;
       } else {
         return false;
       };
-      //CHECK the user's input against correct answer.
-      //Return true or false boolean value.
-      //true if user is correct.
-      //false if user if incorrect.
     };
+    var retract = function retracting() {
+      console.log("trying to retract...");
+      // $(this).removeClass("pop");
+      $(this).closest('div').find('p').removeClass("pop");
+    }.bind(this);
     checkContent();
     //$(this).append("<br>" + );
     console.log("popping out!");
     console.log(this);
     // $(this).addClass("pop");
     $(this).closest('div').find('p').removeClass().addClass("pop");
-    var retract = function retracting() {
-      console.log("trying to retract...");
-      // $(this).removeClass("pop");
-      $(this).closest('div').find('p').removeClass("pop");
-    }.bind(this);
     setTimeout(retract, 2000);
+
   });
 });
