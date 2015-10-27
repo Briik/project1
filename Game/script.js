a8ca1d9615145d63b317f29818549a999f0dfb6f
Bryce Lively <bryce.lively@gmail.com>
2015-10-27 13:14:19 -0400

    found a workaround, now need to add create-a-card

--- Game/script.js
+++ Game/script.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
   console.log("jQuery Working");
   $("#Announcement").html("FLASH CARDS FOR NATIONAL CAPITALS");
+  var WinOrLose = $("#WinOrLose");
   var numCorrect = 0;
-  var gameResponse = "Error: This is the Default Text."
   var correct = "<br><br>CORRECT!<br><br>";
   var tryAgain = "<br><br>Try Again!<br><br>";
+  var gameResponse = "Error: This is the Default Text."
+  var targetP = "placeholder";
+  var savedCapital = "placeholder";
 
   function checkContent(event) {
     var identity = event.target;
-    console.log("2.  identity set to: " + identity);
     var user = prompt("What is the capital of " + event.target.id + "?");
     var capital = event.target.firstChild.nextSibling;
     var theCapital =  capital.textContent.toLowerCase();
-    console.log("3.  Target text: " + theCapital);
     if (user.toLowerCase() == theCapital) {
       numCorrect++;
       gameResponse = correct;
@@ -21,31 +22,39 @@ $(document).ready(function() {
       gameResponse = tryAgain;
       // return false;
     };
-    var oldP = capital.innerHTML;
-    capital.innerHTML = oldP + gameResponse;
-    console.log(capital);
+    WinOrLose.html("" + gameResponse + "");
+  };
+
+  var retract = function retracting(event) {
+    // targetP = event.target.firstChild.nextSibling;
+    targetP.removeClass("pop");
+    WinOrLose.removeClass("pop2");
+    WinOrLose.removeClass("pop");
+    // targetP.innerHTML= savedCapital;
   };
 
   $(".country").on("click", function(event) {
-    console.log("1.  Click detected on " + event.target);
-
+    savedCapital = event.target.firstChild.nextSibling.innerHTML;
     var thisP = $(this).closest('div').find('p')
-    console.log("thisP is " + thisP);
+    // console.log("targetP set to: " + targetP + "  Should be same as thisP.");
 
     var addclass = function addingClass() {
+      WinOrLose.addClass("pop2");
+      WinOrLose.addClass("pop");
       thisP.removeClass().addClass("pop");
     }.bind(this);
 
-    var retract = function retracting() {
-      thisP.removeClass("pop");
-    }.bind(this);
-
     checkContent(event);
+
     addclass();
+    targetP = thisP;
     setTimeout(retract, 2000);
     if (numCorrect === $(".country").length) {
       alert("You have been playing for a while!<br>Why not take a break and go outside?");
     };
+    //this is the return to norm for savedCapital:
+    // event.target.firstChild.nextSibling.innerHTML = savedCapital;
+    // console.log("the return to norm for savedCapital. savedCapital = " + savedCapital);
     console.log("numCorrect: " + numCorrect);
   });
 });
