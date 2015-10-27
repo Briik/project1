5f30dca5ebaf816aab2ae429fc7fe6245edcef85
Bryce Lively <bryce.lively@gmail.com>
2015-10-27 11:00:38 -0400

    functional but need to clear oldP from capital.innerHTML after run

--- Game/script.js
+++ Game/script.js
@@ -2,35 +2,50 @@ $(document).ready(function() {
   console.log("jQuery Working");
   $("#Announcement").html("FLASH CARDS FOR NATIONAL CAPITALS");
   var numCorrect = 0;
+  var gameResponse = "Error: This is the Default Text."
+  var correct = "<br><br>CORRECT!<br><br>";
+  var tryAgain = "<br><br>Try Again!<br><br>";
 
   function checkContent(event) {
     var identity = event.target;
-    console.log("identity set to: " + identity);
+    console.log("2.  identity set to: " + identity);
     var user = prompt("What is the capital of " + event.target.id + "?");
-    var theCapital = event.target.firstChild.nextSibling.textContent.toLowerCase();
-    console.log("Target text: " + theCapital);
+    var capital = event.target.firstChild.nextSibling;
+    var theCapital =  capital.textContent.toLowerCase();
+    console.log("3.  Target text: " + theCapital);
     if (user.toLowerCase() == theCapital) {
       numCorrect++;
-      alert("Good Job!");
-      return true;
+      gameResponse = correct;
+      // return true;
     } else {
-      alert("Try Again...")
-      return false;
+      gameResponse = tryAgain;
+      // return false;
     };
+    var oldP = capital.innerHTML;
+    capital.innerHTML = oldP + gameResponse;
+    console.log(capital);
   };
+
   $(".country").on("click", function(event) {
-    console.log("Click detected on " + event.target.html);
-    var retract = function retracting() {
-      console.log("trying to retract...");
-      $(this).closest('div').find('p').removeClass("pop");
+    console.log("1.  Click detected on " + event.target);
+
+    var thisP = $(this).closest('div').find('p')
+    console.log("thisP is " + thisP);
+
+    var addclass = function addingClass() {
+      thisP.removeClass().addClass("pop");
     }.bind(this);
+
+    var retract = function retracting() {
+      thisP.removeClass("pop");
+    }.bind(this);
+
     checkContent(event);
-    console.log("popping out!");
-    $(this).closest('div').find('p').removeClass().addClass("pop");
+    addclass();
     setTimeout(retract, 2000);
-    console.log("numCorrect: " + numCorrect);
-    if (numCorrect === $(".country").length){
-      alert("You have been playing for a while!");
+    if (numCorrect === $(".country").length) {
+      alert("You have been playing for a while!<br>Why not take a break and go outside?");
     };
+    console.log("numCorrect: " + numCorrect);
   });
 });
