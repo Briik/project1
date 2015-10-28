b4305cac45329e4c6ded3a65d0bb491e0bbb52ee
Bryce Lively <bryce.lively@gmail.com>
2015-10-28 11:55:51 -0400

    working with some basic looks

--- Game/script.js
+++ Game/script.js
@@ -4,50 +4,46 @@ $(document).ready(function() {
   var WinOrLose = $("#WinOrLose");
   var numCorrect = 0;
   var numWrong = 0;
-  var correct = "<br><br>CORRECT!<br><br>";
-  var tryItAgain = "<br><br>Try Again!<br><br>";
+  var correct = "<br>CORRECT!<br><br>";
+  var tryItAgain = "<br>Try Again!<br><br>";
   var gameResponse = "Error: This is the Default Text."
   var targetP = "placeholder";
   var savedCapital = "placeholder";
 
-
-  function cardCreate(){
+  function cardCreate() {
     var cardName = $(".createCountry").val();
     var cardCaptial = $(".createCapital").val();
-    $("#GameSpace").prepend("<div class='country' id='" + cardName + "'>" + cardName + "<p>" + cardCaptial + "</p></div>");
+    $("#GameSpace").prepend("<div class='country' id='" + cardName + "'>" + cardName + "<p class='hidden'>" + cardCaptial + "</p></div>");
     alert("A wild card appeared...");
   };
-// $("#submit").on("click" function(){})
+
   $(".form").submit(function(event) {
     event.preventDefault();
     cardCreate();
   });
 
-
   function checkContent(event) {
     var identity = event.target;
     var user = prompt("What is the capital of " + event.target.id + "?");
     var capital = event.target.firstChild.nextSibling;
-    var theCapital =  capital.textContent.toLowerCase();
+    var theCapital = capital.textContent.toLowerCase();
     if (user.toLowerCase() == theCapital) {
       numCorrect++;
       $("#guessCounter").html("Good Guesses: " + numCorrect);
       gameResponse = correct;
-      // return true;
     } else {
       numWrong++;
       $("#wrongCounter").html("Wrong Answers: " + numWrong);
       gameResponse = tryItAgain;
-      // return false;
     };
     WinOrLose.html("" + gameResponse + "");
   };
 
   var retract = function retracting(event) {
-    // targetP = event.target.firstChild.nextSibling;
     targetP.removeClass("pop");
+    targetP.addClass("hidden");
     WinOrLose.removeClass("pop2");
-    WinOrLose.removeClass("pop");
+    // WinOrLose.removeClass("pop");
     //Commented out due to classes not being added... :(
     // WinOrLose.removeClass("correct");
     // WinOrLose.removeClass("tryAgain");
@@ -57,7 +53,7 @@ $(document).ready(function() {
   $("body").on("click", ".country", function(event) {
     savedCapital = event.target.firstChild.nextSibling.innerHTML;
     var thisP = $(this).closest('div').find('p')
-    // console.log("targetP set to: " + targetP + "  Should be same as thisP.");
+      // console.log("targetP set to: " + targetP + "  Should be same as thisP.");
 
     var addclass = function addingClass() {
       WinOrLose.addClass("pop2");
@@ -68,20 +64,16 @@ $(document).ready(function() {
       // if (WinOrLose.innerHTML ==tryItAgain) {
       //   WinOrLose.addClass("tryAgain");
       // };
-      thisP.removeClass().addClass("pop");
+      thisP.removeClass("hidden").addClass("pop");
     }.bind(this);
 
     checkContent(event);
-
     addclass();
     targetP = thisP;
     setTimeout(retract, 2000);
     if (numCorrect === $(".country").length) {
       alert("You have been playing for a while!<br>Why not take a break and go outside?");
     };
-    //this is the return to norm for savedCapital:
-    // event.target.firstChild.nextSibling.innerHTML = savedCapital;
-    // console.log("the return to norm for savedCapital. savedCapital = " + savedCapital);
     console.log("numCorrect: " + numCorrect);
   });
 });
